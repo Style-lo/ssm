040a765dc6be19016045cf00576534987713c776
Style-lo <375277869@qq.com>
2017-12-21 10:19:00 +0800

    Date

--- src/main/java/com/ssm/api/test/TestDate.java
+++ src/main/java/com/ssm/api/test/TestDate.java
@@ -2,6 +2,7 @@ package com.ssm.api.test;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import org.junit.Test;
@@ -43,5 +44,63 @@ public class TestDate {
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
+	}
+	/**
+	 * 获取指定时间的时间戳
+	 * @param hh	时
+	 * @param mm	分
+	 * @param ss	秒
+	 * @return	时间戳
+	 */
+	public static long getCurrentTime(int hh,int mm, int ss) {
+		Calendar calendar2 = Calendar.getInstance();
+		calendar2.set(calendar2.get(Calendar.YEAR), calendar2.get(Calendar.MONTH),
+				calendar2.get(Calendar.DAY_OF_MONTH), hh, mm, ss);
+		Date endOfDate = calendar2.getTime();
+		return endOfDate.getTime() / 1000;
+	
+	}
+	/*public static void main(String[] args) {
+		long currentTime = getCurrentTime(9,0,0);
+		long currentTime2 = getCurrentTime(11,59,59);
+		System.out.println(currentTime);
+		System.out.println("11:"+currentTime2);
+	}*/
+	@Test
+	public void s3(){
+		String date = "2017-11-20 20:30";
+		SimpleDateFormat sldf = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+		try {
+			Date parse = sldf.parse(date);
+			long ss = parse.getTime()/1000;
+			System.out.println(ss);
+			System.out.println(parse);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void s4(Date date,int hh,int mm,int ss){
+		Calendar day = Calendar.getInstance();
+		day.setTime(date);
+		day.set(day.get(Calendar.YEAR), day.get(Calendar.MONTH),
+				day.get(Calendar.DAY_OF_MONTH), hh, mm, ss);
+		Date time = day.getTime();
+		long time2 = time.getTime()/1000;
+		System.out.println(time);
+		System.out.println(time2);
+	}
+	public static void main(String[] args) {
+		String ss = "2017-11-05 23:00";
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+		Date parse=null;
+		try {
+			parse = sdf.parse(ss);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		s4(parse, 21 ,0,0);
 	}
 }

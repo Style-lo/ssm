956f7b47f36b1cbd31f908dde128e99941ac1d21
Style-lo <375277869@qq.com>
2018-03-01 16:23:35 +0800

    update

---
+++ src/main/java/com/ssm/api/utils/IOUtils.java
@@ -0,0 +1,23 @@
+package com.ssm.api.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class IOUtils {
+
+    /**
+     * 读取资源
+     */
+    /*public static byte[] readResource(String resource){
+        byte[] buffer = null;
+        try {
+            InputStream certStream = IOUtils.class.getResourceAsStream(resource);
+            buffer = new byte[certStream.available()];
+            certStream.read(buffer);
+            certStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return buffer;
+    }*/
+}
